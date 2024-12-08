916dcf4d3b176bc778bfef5a34239ce6222cbe38
Carlos <carlosifsm@gmail.com>
2024-12-08 17:45:48 -0300

    feat: add botaoRetorno em Criar/Editar Plano

---
+++ frontend/src/components/BotaoRetorno.js
@@ -0,0 +1,8 @@
+
+export default function BotaoRetorno(){
+    return(
+        <div className="size-7 mb-5">
+            <img className="size-max cursor-pointer" src="/images/seta_esquerda.png" alt="" />
+        </div>
+    )
+}
--- frontend/src/pages/CriarPlano.js
+++ frontend/src/pages/CriarPlano.js
@@ -1,20 +1,16 @@
 import InfoPlanoCriar from "../components/CriarEditarPlano/InfoPlanoCriar";
-
-function formatSectionName(name){
-    return name.padEnd(34, '.');
-}
+import { Link } from "react-router-dom";
+import BotaoRetorno from "../components/BotaoRetorno";
+import TitleSection from "../components/PedidosGerente/TitleSection";
 
 
 export default function CriarPlano(){
 
     return(<>
-        <div className="flex items-center mb-6 gap-3">
-            <img src="/images/assinatura.png" alt="" className="size-8"/>
-            <span>
-                <h1 className="text-2xl font-semibold text-secondaryBlue">{formatSectionName("Criar Plano de Assinatura")}</h1>
-            </span>
-        </div>
-        
+        <Link to="/gerente/planos">
+            <BotaoRetorno/>
+        </Link>
+        <TitleSection sectionName={"Criar Plano de Assinatura"} img={"/images/assinatura.png"}></TitleSection>
         <InfoPlanoCriar></InfoPlanoCriar>
 
     </>)
