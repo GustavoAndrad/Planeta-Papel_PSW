a8ae6f46b96c9379df360484ff90ffc76c3bfab9
Gustavo  Barros <gustavoandrade0125@gmail.com>
2024-12-09 22:56:10 -0300

    fix: remove func. de Carrinho do adm

--- frontend/src/components/Catalogo/Propaganda.js
+++ frontend/src/components/Catalogo/Propaganda.js
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 function Propaganda(props) {
 
+    const isGerente = localStorage.getItem("gerente")==='true'? true : false;
+    const isLogged = localStorage.getItem("id");
+    const redirect = isLogged? (isGerente? "/gerente/planos" : "/cliente/planos") : "/login";
+
     return (
       <>
         <div className="mt-[3rem] mb-[3rem] w-full h-auto flex justify-center items-center"> 
-            <Link className="cursor-pointer bg-primaryBlue py-[0%] sm:py-[2%] min-w-[300px] w-2/3 h-full rounded-3xl sm:flex sm:justify-center sm:items-center]" to={`/planos/${props.planoName}`} replace>
+            <Link className="cursor-pointer bg-primaryBlue py-[0%] sm:py-[2%] min-w-[300px] w-2/3 h-full rounded-3xl sm:flex sm:justify-center sm:items-center]" to={redirect} replace>
 
                 <div className=" w-full sm:w-1/2 min-h-[100px] border-whit border-b-4 sm:border-b-0 sm:border-r-4 flex items-center justify-center text-3xl text-white font-bold text-center">
                     PLANO {props.planoName.toString().toUpperCase()}
