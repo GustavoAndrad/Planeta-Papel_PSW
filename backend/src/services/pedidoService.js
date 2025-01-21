bfb6ba65e48226d78d8e45f1185bf3448817ae52
laraFRamos <lara.fernandes.ramos2003@gmail.com>
2025-01-21 20:51:17 -0300

    Criação do protótipo do service de pedido

--- backend/src/services/pedidoService.js
+++ backend/src/services/pedidoService.js
@@ -1,17 +1,24 @@
+const Pedido = require('../models/pedido');
 
 async function createPedido(pedidoData) {
+    const pedido = new Pedido(pedidoData);
+    return await pedido.save();
 }
 
 async function getPedidoById(id) {
+    return await Pedido.findById(id);
 }
 
 async function getPedidos() {
+    return await Pedido.find();
 }
 
 async function updatePedido(id, pedidoData) {
+    return await Pedido.findByIdAndUpdate(id, pedidoData, { new: true, runValidators: true });
 }
 
 async function deletePedido(id) {
+    return await Pedido.findByIdAndDelete(id);
 }
 
 module.exports = {
@@ -20,4 +27,4 @@ module.exports = {
     getPedidoById,
     updatePedido,
     deletePedido
-}
+};
