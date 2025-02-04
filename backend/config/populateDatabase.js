1b07a405e2ec7dfeb3cd25a2f6bae2cf6cc72934
Erick <135640111+erick1-618@users.noreply.github.com>
2025-02-03 22:16:14 -0300

    feat: add catalog demo products and images

--- backend/config/populateDatabase.js
+++ backend/config/populateDatabase.js
@@ -9,44 +9,232 @@ const Plano = require('../src/models/plano.js');
  */
 
 const produtos = [
-    {
-      "id": "1",
-      "nome": "Folha A4",
-      "preco": 10,
-      "descricao": "Pacote com 5 folhas de papel sulfite A4",
-      "imagem": [
-        "/images/prod.png"
-      ],
-      "qntDisponivel": 1,
-      "categoria": "Papel"
+  {
+    "id": "2",
+    "nome": "Papel Colorido A4 - 100 folhas",
+    "preco": 12.9,
+    "descricao": "Pacote com 100 folhas de papel colorido A4, disponível em várias cores.",
+    "imagem": [
+      "papel1.jpeg"
+    ],
+    "qntDisponivel": 50,
+    "categoria": "Papel"
+  },
+  {
+    "id": "3",
+    "nome": "Papel Cartão - 25 folhas",
+    "preco": 1,
+    "descricao": "Papel cartão resistente para uso em trabalhos manuais, scrapbooking e outros.",
+    "imagem": [
+      "papel2.jpeg"
+    ],
+    "qntDisponivel": 6,
+    "categoria": "Papel"
+  },
+  {
+    "id": "5",
+    "nome": "Papel Couchê Glossy A4 - 200 folhas",
+    "preco": 35.9,
+    "descricao": "Pacote com 200 folhas de papel couchê glossy A4, perfeito para impressões de alta qualidade.",
+    "imagem": [
+      "papel3.jpeg"
+    ],
+    "qntDisponivel": 30,
+    "categoria": "Papel"
+  },
+  {
+    "id": "6",
+    "nome": "Papel Fotográfico A4 - 100 folhas",
+    "preco": 45.9,
+    "descricao": "Papel fotográfico A4 para impressões de fotos, com excelente qualidade de imagem.",
+    "imagem": [
+      "papel1.jpeg",
+      "papel2.jpeg"
+    ],
+    "qntDisponivel": 80,
+    "categoria": "Papel"
+  },
+  {
+    "id": "7",
+    "nome": "Papel Offset A4 - 1000 folhas",
+    "preco": 55.9,
+    "descricao": "Pacote com 1000 folhas de papel offset A4, ideal para uso comercial.",
+    "imagem": [
+      "papel3.jpeg",
+      "papel2.jpeg",
+      "papel1.jpeg"
+    ],
+    "qntDisponivel": 200,
+    "categoria": "Papel"
     },
     {
-      "id": "2",
-      "nome": "Papel Colorido A4 - 100 folhas",
+      "id": "8",
+      "nome": "Post-it 76mm x 76mm - Bloco com 100 folhas",
+      "preco": 9.9,
+      "descricao": "Bloco de post-it tamanho 76mm x 76mm, perfeito para anotações rápidas.",
+      "imagem": [
+        "postit1.jpeg"
+      ],
+      "qntDisponivel": 200,
+      "categoria": "Post-it"
+    },
+    {
+      "id": "9",
+      "nome": "Post-it Super Sticky 76mm x 76mm - Bloco com 90 folhas",
+      "preco": 14.9,
+      "descricao": "Post-it super sticky que gruda mais e é ideal para papéis e superfícies diversas.",
+      "imagem": [
+        "postit2.jpeg"
+      ],
+      "qntDisponivel": 120,
+      "categoria": "Post-it"
+    },
+    {
+      "id": "10",
+      "nome": "Post-it 50mm x 50mm - Bloco com 150 folhas",
+      "preco": 11.9,
+      "descricao": "Post-it com 150 folhas no tamanho 50mm x 50mm, excelente para uso escolar e de escritório.",
+      "imagem": [
+        "postit3.jpeg"
+      ],
+      "qntDisponivel": 180,
+      "categoria": "Post-it"
+    },
+    {
+      "id": "11",
+      "nome": "Post-it Neon 76mm x 76mm - Bloco com 100 folhas",
+      "preco": 13.9,
+      "descricao": "Bloco de post-it neon para destacar suas anotações, com 100 folhas.",
+      "imagem": [
+        "postit1.jpeg"
+      ],
+      "qntDisponivel": 150,
+      "categoria": "Post-it"
+    },
+    {
+      "id": "12",
+      "nome": "Post-it 76mm x 76mm - Bloco com 50 folhas",
+      "preco": 7.9,
+      "descricao": "Bloco de post-it tamanho 76mm x 76mm com 50 folhas.",
+      "imagem": [
+        "postit2.jpeg",
+        "postit1.jpeg",
+        "postit3.jpeg"
+      ],
+      "qntDisponivel": 200,
+      "categoria": "Post-it"
+    },
+    {
+      "id": "16",
+      "nome": "Cartolina Colorida A3 - 10 folhas",
+      "preco": 10.9,
+      "descricao": "Pacote com 10 folhas de cartolina colorida A3, excelente para trabalhos escolares e artes.",
+      "imagem": [
+        "cartolina1.jpeg"
+      ],
+      "qntDisponivel": 80,
+      "categoria": "Cartolina"
+    },
+    {
+      "id": "17",
+      "nome": "Cartolina Branca A3 - 10 folhas",
+      "preco": 8.9,
+      "descricao": "Pacote de 10 folhas de cartolina branca A3, versátil para projetos e apresentações.",
+      "imagem": [
+        "cartolina2.jpeg"
+      ],
+      "qntDisponivel": 60,
+      "categoria": "Cartolina"
+    },
+    {
+      "id": "18",
+      "nome": "Cartolina Colorida A4 - 25 folhas",
       "preco": 12.9,
-      "descricao": "Pacote com 100 folhas de papel colorido A4, disponível em várias cores.",
+      "descricao": "Cartolina colorida A4, com 25 folhas para criar projetos criativos.",
       "imagem": [
-        "/images/prod.png",
-        "/images/prod.png",
-        "/images/prod.png",
-        "/images/prod.png"
+        "cartolina3.jpeg"
       ],
-      "qntDisponivel": 50,
-      "categoria": "Papel"
+      "qntDisponivel": 100,
+      "categoria": "Cartolina"
     },
     {
-      "id": "3",
-      "nome": "Papel Cartão - 25 folhas",
-      "preco": 1,
-      "descricao": "Papel cartão resistente para uso em trabalhos manuais, scrapbooking e outros.",
+      "id": "19",
+      "nome": "Cartolina Branca A4 - 25 folhas",
+      "preco": 10.9,
+      "descricao": "Cartolina branca A4, pacote com 25 folhas.",
       "imagem": [
-        "/images/prod.png",
-        "/images/prod.png",
-        "/images/prod.png",
-        "/images/prod.png"
+        "cartolina1.jpeg"
       ],
-      "qntDisponivel": 6,
-      "categoria": "Papel"
+      "qntDisponivel": 80,
+      "categoria": "Cartolina"
+    },
+    {
+      "id": "20",
+      "nome": "Cartolina Colorida A3 - 5 folhas",
+      "preco": 7.9,
+      "descricao": "Cartolina colorida A3, com 5 folhas para pequenas criações.",
+      "imagem": [
+        "cartolina2.jpeg",
+        "cartolina1.jpeg"
+      ],
+      "qntDisponivel": 60,
+      "categoria": "Cartolina"
+    },
+    {
+      "id": "24",
+      "nome": "Caderno Universitário - 80 folhas",
+      "preco": 19.9,
+      "descricao": "Caderno universitário com capa dura e 80 folhas, ideal para anotações no dia a dia.",
+      "imagem": [
+        "caderno1.jpeg"
+      ],
+      "qntDisponivel": 150,
+      "categoria": "Caderno"
+    },
+    {
+      "id": "25",
+      "nome": "Caderno Espiral - 100 folhas",
+      "preco": 23.9,
+      "descricao": "Caderno espiral com 100 folhas, capa dura e formato A5, ideal para o uso escolar e profissional.",
+      "imagem": [
+        "caderno2.jpeg"
+      ],
+      "qntDisponivel": 120,
+      "categoria": "Caderno"
+    },
+    {
+      "id": "26",
+      "nome": "Caderno Universitário 120 folhas",
+      "preco": 28.9,
+      "descricao": "Caderno universitário 120 folhas, ideal para uso em cursos técnicos e faculdade.",
+      "imagem": [
+        "caderno3.jpeg"
+      ],
+      "qntDisponivel": 90,
+      "categoria": "Caderno"
+    },
+    {
+      "id": "27",
+      "nome": "Caderno Pautado - 200 folhas",
+      "preco": 35.9,
+      "descricao": "Caderno pautado de 200 folhas, ideal para escritores e estudantes.",
+      "imagem": [
+        "caderno1.jpeg"
+      ],
+      "qntDisponivel": 60,
+      "categoria": "Caderno"
+    },
+    {
+      "id": "28",
+      "nome": "Caderno Escolar - 60 folhas",
+      "preco": 16.9,
+      "descricao": "Caderno escolar com 60 folhas pautadas, ideal para o uso diário.",
+      "imagem": [
+        "caderno2.jpeg",
+        "caderno1.jpeg"
+      ],
+      "qntDisponivel": 200,
+      "categoria": "Caderno"
     }
 ]
 
